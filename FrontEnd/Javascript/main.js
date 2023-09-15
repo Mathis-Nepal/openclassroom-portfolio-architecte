e6eebae0c27a1da613b296c40a9cb872c29d4a84
Nepal Mathis <mathis.nepal@mediaschool.me>
2023-09-15 13:04:30 +0200

    [MIN] finish project

--- FrontEnd/Javascript/main.js
+++ FrontEnd/Javascript/main.js
@@ -1,73 +1,20 @@
 import { MODE } from "./config.js";
+import { createElementWithClassesAndText } from "./utils.js";
+import { ViewAdmin } from "./utils.js";
+import { createFirstModal } from "./utils.js";
+import { createSecondModal } from "./utils.js";
+import { createCustomInputFile } from "./utils.js";
 
-//session plutot que local storage
-// const responseWorks = JSON.parse(localStorage.getItem("response"));
 const responseWorks = await fetch(`${MODE}/works`).then((response) => response.json());
-
-// async function BackEndRessources(newData = false) {
-// 	try {
-// 		if (newData) {
-// 			responseWorks = null;
-// 		}
-// 		if (responseWorks === null) {
-// 			console.log("affect response to localStorage");
-// 			const response = await fetch(`${MODE}/works`).then((response) => response.json());
-// 			localStorage.setItem("response", JSON.stringify(response));
-// 			responseWorks = localStorage.getItem("response");
-// 		} else {
-// 			console.log("already existing in localStorage");
-// 		}
-// 	} catch (error) {
-// 		console.log(error);
-// 	}
-// }
-
-// await BackEndRessources();
-
-let createModalBool = true;
-
-const createElementWithClasses = (tag, classes) => {
-	const element = document.createElement(tag);
-	element.classList.add(...classes);
-	return element;
-
-	// element.id.add(...id);
-};
-
-// Fonction pour créer un élément avec des classes et un éventuel texte
-const createElementWithClassesAndText = (tag, classes, text) => {
-	const element = document.createElement(tag);
-	element.classList.add(...classes);
-	if (text) {
-		element.textContent = text;
-	}
-	return element;
-};
-
-const listFilter = document.querySelector(".list-filter");
-const editionMode = document.querySelector(".edition-mode");
-const editionModeButton = document.querySelectorAll(".edition-mode-button");
-// localStorage.removeItem("token");
-// localStorage.removeItem("response");
 const JWTtoken = sessionStorage.getItem("token");
+let createModalBool = true;
+const editionModeButton = document.querySelectorAll(".edition-mode-button");
 
-const admin = JWTtoken !== null;
+ViewAdmin(JWTtoken, editionModeButton);
 
-if (listFilter !== null && editionMode !== null && editionModeButton !== null) {
-	if (admin) {
-		listFilter.classList.add("hidden");
-		editionMode.classList.remove("hidden");
-		editionModeButton.forEach((button) => {
-			button.classList.remove("hidden");
-		});
-	} else {
-		listFilter.classList.remove("hidden");
-		editionMode.classList.add("hidden");
-		editionModeButton.forEach((button) => {
-			button.classList.add("hidden");
-		});
-	}
-}
+editionModeButton.forEach((button) => {
+	button.addEventListener("click", handleEditionModeClick);
+});
 
 //get all the images from the server and display them
 const gallery = document.querySelector(".gallery");
@@ -75,7 +22,7 @@ let galleryAdmin = document.querySelector(".admin-gallery");
 
 function displayImages(response, admin = false) {
 	if (galleryAdmin === null) {
-		galleryAdmin = createElementWithClasses("div", ["admin-gallery"]);
+		galleryAdmin = createElementWithClassesAndText("div", ["admin-gallery"]);
 	}
 	const container = admin ? galleryAdmin : gallery;
 
@@ -88,7 +35,7 @@ function displayImages(response, admin = false) {
 		if (!admin) {
 			const figcaption = document.createElement("figcaption");
 			figcaption.textContent = element.title;
-			figure.append(figcaption, img);
+			figure.append(img, figcaption);
 		} else {
 			const trash = document.createElement("button");
 			trash.classList.add("trash");
@@ -112,7 +59,6 @@ function deleteImage(figure) {
 		indexElementDelete.push(figure.dataset.id);
 		figure.remove();
 	});
-	// BackEndRessources(true);
 }
 
 //filter
@@ -146,42 +92,37 @@ if (gallery !== null) {
 
 //connection
 
-const email = document.querySelector(".email-connection");
-const password = document.querySelector(".password-connection");
-const buttonConnection = document.querySelector(".connection");
-const error = document.querySelector(".error-connection");
+function connection() {
+	const email = document.querySelector(".email-connection");
+	const password = document.querySelector(".password-connection");
+	const buttonConnection = document.querySelector(".connection");
+	const error = document.querySelector(".error-connection");
 
-if (buttonConnection !== null) {
-	buttonConnection.addEventListener("click", async (event) => {
-		event.preventDefault();
-		const emailValue = email.value;
-		const passwordValue = password.value;
-		fetch(`${MODE}/users/login`, {
-			method: "POST",
-			headers: { "Content-Type": "application/json" },
-			body: JSON.stringify({ email: emailValue, password: passwordValue }),
-		}).then(async (response) => {
-			const token = await response.json().then((data) => data.token);
-			if (response.status === 200) {
-				window.location.href = "index.html";
-				sessionStorage.setItem("token", token);
-			} else {
-				error.classList.add("active");
-			}
+	if (buttonConnection !== null) {
+		buttonConnection.addEventListener("click", async (event) => {
+			event.preventDefault();
+			const emailValue = email.value;
+			const passwordValue = password.value;
+			fetch(`${MODE}/users/login`, {
+				method: "POST",
+				headers: { "Content-Type": "application/json" },
+				body: JSON.stringify({ email: emailValue, password: passwordValue }),
+			}).then(async (response) => {
+				const token = await response.json().then((data) => data.token);
+				if (response.status === 200) {
+					window.location.href = "index.html";
+					sessionStorage.setItem("token", token);
+				} else {
+					error.classList.add("active");
+				}
+			});
 		});
-	});
+	}
 }
+connection();
 
 //modal amdin
 
-// displayImages(response, true);
-
-// homeModal.showModal();
-// homeModal.classList.remove("hidden");
-// firstModalContent.classList.add("hidden");
-// secondModalContent.classList.remove("hidden");
-
-// const homeModal = document.querySelector(".container-modal");
 let closeModalButtons = document.querySelectorAll(".close-modal");
 let homeModal = document.querySelector(".container-modal");
 let modalContent = document.querySelectorAll(".modal-content");
@@ -189,14 +130,10 @@ let firstModalContent = document.querySelector(".modal-content.first");
 let secondModalContent = document.querySelector(".modal-content.second");
 let bodyElement = document.querySelector("body");
 let buttonAddImage = document.querySelector(".button.modal");
-
-editionModeButton.forEach((button) => {
-	button.addEventListener("click", handleEditionModeClick);
-});
+let inputFile = null;
 
 function handleEditionModeClick(event) {
 	if (createModalBool === true) {
-		console.log("create modal");
 		createModal();
 		createModalBool = false;
 	}
@@ -208,14 +145,12 @@ function handleEditionModeClick(event) {
 	firstModalContent = document.querySelector(".modal-content.first");
 	secondModalContent = document.querySelector(".modal-content.second");
 	bodyElement = document.querySelector("body");
-	buttonAddImage = document.querySelector(".button.modal");
+	buttonAddImage = document.querySelectorAll(".button.modal");
 
 	homeModal.classList.remove("hidden");
 	firstModalContent.classList.remove("hidden");
 	secondModalContent.classList.add("hidden");
 	homeModal.showModal();
-
-	// openHomeModal();
 
 	bodyElement.style.overflow = "hidden";
 	event.stopPropagation();
@@ -231,31 +166,91 @@ function handleEditionModeClick(event) {
 		button.addEventListener("click", closingModal);
 	});
 
-	buttonAddImage.addEventListener("click", () => {
+	buttonAddImage[0].addEventListener("click", () => {
 		firstModalContent.classList.add("hidden");
 		secondModalContent.classList.remove("hidden");
 	});
+
+	// formulaire d'ajout d'image
+
+	if (secondModalContent !== null) {
+		const formulaire = secondModalContent.querySelector("form");
+		inputFile = formulaire.querySelector("#file");
+		const customInputFile = formulaire.querySelector(".button-file .container");
+		const input = formulaire.querySelector("#title");
+		const select = formulaire.querySelector("#categories");
+
+		input.addEventListener("input", verifyChampCompleted);
+		select.addEventListener("input", verifyChampCompleted);
+		inputFile.addEventListener("input", verifyChampCompleted);
+
+		function verifyChampCompleted() {
+			if (inputFile.files.length > 0 && input.value !== "" && select.value !== "") {
+				buttonAddImage[1].classList.remove("disabled");
+				formulaire.addEventListener("submit", async (event) => {
+					const image = document.querySelector(".preview-image");
+					event.preventDefault();
+					const formData = new FormData(formulaire);
+					const select = formulaire.querySelector("#categories");
+					const optionElement = select.querySelector(`option[value="${select.value}"]`);
+
+					formData.append("category", optionElement.dataset.id);
+
+					const response = await fetch(`${MODE}/works`, {
+						method: "POST",
+						headers: { Authorization: `Bearer ${JWTtoken}` },
+						body: formData,
+					}).then((response) => response.json());
+				});
+			} else {
+				buttonAddImage[1].classList.add("disabled");
+			}
+		}
+
+		inputFile.addEventListener("change", () => {
+			customInputFile.innerHTML = "";
+
+			// Obtient les fichiers sélectionnés par l'utilisateur
+			const files = inputFile.files;
+
+			// Vérifie s'il y a au moins un fichier sélectionné
+			if (files.length > 0) {
+				// Crée un objet FileReader pour lire le contenu du fichier
+				const reader = new FileReader();
+
+				// Crée un élément image
+				const image = document.createElement("img");
+				image.classList.add("preview-image"); // Ajoute une classe pour le style CSS
+				customInputFile.appendChild(image);
+
+				// Ajoute un écouteur d'événement pour lire le fichier lorsqu'il est chargé
+				reader.addEventListener("load", () => {
+					// Affecte la source de l'image à la donnée lue depuis le fichier
+					image.src = reader.result;
+				});
+
+				// Lit le contenu du fichier en tant que données URL
+				reader.readAsDataURL(files[0]);
+			} else {
+				createCustomInputFile(customInputFile);
+			}
+		});
+	}
 }
 
 function createModal() {
-	console.log("create modal ici ");
-	const dialog = createElementWithClasses("dialog", ["container-modal", "hidden"]);
-	createFirstModal(dialog);
-	createSecondModal(dialog);
+	const dialog = createElementWithClassesAndText("dialog", ["container-modal", "hidden"]);
+	createFirstModal(dialog, galleryAdmin);
+	createSecondModal(dialog, responseWorks);
 	document.body.appendChild(dialog);
 }
-
-// function openHomeModal() {
-// 	homeModal.classList.remove("hidden");
-// 	firstModalContent.classList.remove("hidden");
-// 	secondModalContent.classList.add("hidden");
-// 	homeModal.showModal();
 
 function closingModal() {
 	galleryAdmin.innerHTML = "";
 	homeModal.classList.add("hidden");
 	homeModal.close();
 	bodyElement.style.overflow = "auto";
+
 	indexElementDelete.forEach(async (index) => {
 		await fetch(`${MODE}/works/${index}`, {
 			method: "DELETE",
@@ -263,86 +258,4 @@ function closingModal() {
 		});
 	});
 	indexElementDelete = [];
-}
-
-// formulaire d'ajout d'image
-
-if (secondModalContent !== null) {
-	const formulaire = secondModalContent.querySelector("form");
-	const inputFile = formulaire.querySelector("#file");
-	const customInputFile = formulaire.querySelector(".button-file .container");
-
-	//when user seleet file
-
-	inputFile.addEventListener("change", () => {
-		customInputFile.innerHTML = "";
-		const file = inputFile.files;
-		const reader = new FileReader();
-		const image = document.createElement("img");
-		image.src = file[0];
-		console.log(file[0]);
-		customInputFile.appendChild(image);
-		reader.addEventListener("load", () => {});
-	});
-}
-
-//créer le premier modal
-
-function createFirstModal(dialog) {
-	console.log("create first modal dans le first create modal");
-
-	const modalContent = createElementWithClasses("div", ["modal-content", "first"]);
-	const i = createElementWithClasses("i", ["fa-solid", "fa-xmark", "close-modal"]);
-	const titleModal = createElementWithClasses("span", ["title-modal"], "Galerie photo");
-	if (galleryAdmin !== null) {
-		// const adminGallery = createElementWithClasses("div", ["admin-gallery"]);
-		galleryAdmin.classList.add("admin-gallery");
-	} else {
-		galleryAdmin = createElementWithClasses("div", ["admin-gallery"]);
-	}
-	const separator = createElementWithClasses("span", ["separator"]);
-	const buttonModal = createElementWithClassesAndText("button", ["button", "modal"], "Ajouter une photo");
-	modalContent.append(i, titleModal, galleryAdmin, separator, buttonModal);
-	console.log(modalContent);
-	dialog.appendChild(modalContent);
-	// document.body.appendChild(dialog);
-}
-
-function createSecondModal(dialog) {
-	const formStyleElement = createElementWithClassesAndText("div", ["modal-content", "second", "hidden"]);
-	formStyleElement.id = "form-style";
-
-	// Créer et ajouter les éléments du formulaire au div form-style
-	formStyleElement.append(createElementWithClassesAndText("i", ["fa-solid", "fa-xmark", "close-modal"]), createElementWithClassesAndText("span", ["title-modal"], "Ajout photo"));
-
-	// Créer le formulaire
-	const formElement = createElementWithClassesAndText("form", [], null);
-	formElement.action = "#"; // Spécifiez l'URL de l'action du formulaire ici
-
-	formElement.append(
-		createElementWithClassesAndText("button", ["button-file"], null),
-		createElementWithClassesAndText("label", [], "+ Ajouter photo"),
-		createElementWithClassesAndText("input", [], null),
-		createElementWithClassesAndText("label", [], "Titre"),
-		createElementWithClassesAndText("input", ["shadow"], null),
-		createElementWithClassesAndText("label", [], "Categorie")
-	);
-
-	// Créer le menu déroulant
-	const categoriesSelectElement = createElementWithClassesAndText("select", ["shadow"], null);
-	categoriesSelectElement.id = "categories";
-
-	const categories = ["Objets", "Appartements", "Hôtels & restaurants"];
-
-	categories.forEach((category) => {
-		const optionElement = createElementWithClassesAndText("option", [], category);
-		optionElement.value = category;
-		categoriesSelectElement.appendChild(optionElement);
-	});
-	const button = createElementWithClassesAndText("input", ["button", "modal", "disabled"]);
-	button.value = "Ajouter";
-	formElement.append(categoriesSelectElement, createElementWithClassesAndText("span", ["separator"], null), button);
-
-	formStyleElement.appendChild(formElement);
-	dialog.appendChild(formStyleElement);
 }
---
+++ FrontEnd/Javascript/utils.js
@@ -0,0 +1,107 @@
+export const createElementWithClassesAndText = (tag, classes, text) => {
+	const element = document.createElement(tag);
+	element.classList.add(...classes);
+	if (text) {
+		element.textContent = text;
+	}
+	return element;
+};
+
+export function ViewAdmin(JWTtoken, editionModeButton) {
+	const admin = JWTtoken !== null;
+	const listFilter = document.querySelector(".list-filter");
+	const editionMode = document.querySelector(".edition-mode");
+	if (listFilter !== null && editionMode !== null && editionModeButton !== null) {
+		if (admin) {
+			listFilter.classList.add("hidden");
+			editionMode.classList.remove("hidden");
+			editionModeButton.forEach((button) => {
+				button.classList.remove("hidden");
+			});
+		} else {
+			listFilter.classList.remove("hidden");
+			editionMode.classList.add("hidden");
+			editionModeButton.forEach((button) => {
+				button.classList.add("hidden");
+			});
+		}
+	}
+}
+
+export function createFirstModal(dialog, galleryAdmin) {
+	const modalContent = createElementWithClassesAndText("div", ["modal-content", "first"]);
+	const i = createElementWithClassesAndText("i", ["fa-solid", "fa-xmark", "close-modal"]);
+	const titleModal = createElementWithClassesAndText("span", ["title-modal"], "Galerie photo");
+	if (galleryAdmin !== null) {
+		galleryAdmin.classList.add("admin-gallery");
+	} else {
+		galleryAdmin = createElementWithClassesAndText("div", ["admin-gallery"]);
+	}
+	const separator = createElementWithClassesAndText("span", ["separator"]);
+	const buttonModal = createElementWithClassesAndText("button", ["button", "modal"], "Ajouter une photo");
+	modalContent.append(i, titleModal, galleryAdmin, separator, buttonModal);
+	dialog.appendChild(modalContent);
+}
+
+export function createSecondModal(dialog, responseWorks) {
+	const formStyleElement = createElementWithClassesAndText("div", ["modal-content", "second", "hidden"]);
+	formStyleElement.id = "form-style";
+
+	// Créer et ajouter les éléments du formulaire au div form-style
+	formStyleElement.append(createElementWithClassesAndText("i", ["fa-solid", "fa-xmark", "close-modal"]), createElementWithClassesAndText("span", ["title-modal"], "Ajout photo"));
+
+	// Créer le formulaire
+	const formElement = createElementWithClassesAndText("form", [], null);
+	formElement.action = "#"; // Spécifiez l'URL de l'action du formulaire ici
+
+	const buttonFile = createElementWithClassesAndText("button", ["button-file"], null);
+	buttonFile.type = "button";
+	createCustomInputFile(buttonFile);
+	const inputTitle = createElementWithClassesAndText("input", ["shadow"], null);
+	inputTitle.type = "text";
+	inputTitle.id = "title";
+	inputTitle.name = "title";
+	inputTitle.placeholder = "Abajour Tahina";
+	formElement.append(buttonFile, createElementWithClassesAndText("label", [], "Titre"), inputTitle, createElementWithClassesAndText("label", [], "Categorie"));
+
+	// Créer le menu déroulant
+	const categoriesSelectElement = createElementWithClassesAndText("select", ["shadow"], null);
+	categoriesSelectElement.id = "categories";
+
+	const categories = responseWorks.map((element) => element.category.name);
+	const categoriesUnique = [...new Set(categories)];
+
+	const optionElement = createElementWithClassesAndText("option", [], "");
+	optionElement.value = "";
+	categoriesSelectElement.appendChild(optionElement);
+	let counterCategories = 1;
+	categoriesUnique.forEach((category) => {
+		const optionElement = createElementWithClassesAndText("option", [], category);
+		optionElement.dataset.id = counterCategories;
+		optionElement.value = category;
+		categoriesSelectElement.appendChild(optionElement);
+		counterCategories++;
+	});
+
+	const button = createElementWithClassesAndText("input", ["button", "modal", "disabled"]);
+	button.type = "submit";
+	button.value = "Ajouter";
+	formElement.append(categoriesSelectElement, createElementWithClassesAndText("span", ["separator"], null), button);
+
+	formStyleElement.appendChild(formElement);
+	dialog.appendChild(formStyleElement);
+}
+
+export function createCustomInputFile(buttonFile) {
+	const container = createElementWithClassesAndText("div", ["container"]);
+	const label = createElementWithClassesAndText("label", [], "+ Ajouter une photo");
+	label.for = "file";
+	container.append(createElementWithClassesAndText("i", ["fa-solid", "fa-image"]), label, createElementWithClassesAndText("span", [], "jpg, png : 4mo max"));
+	const input = createElementWithClassesAndText("input", [], null);
+	input.type = "file";
+	input.id = "file";
+	input.name = "image";
+	input.accept = "image/png, image/jpeg";
+	input.required = true;
+	buttonFile.append(container, input);
+}
